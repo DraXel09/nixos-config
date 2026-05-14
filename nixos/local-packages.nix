8b070351f23daddb5bf81b73928f1e7407f47085
Joelly Bedoya <bedoyajoelly@gmail.com>
2026-05-13 23:26:40 -0500

    feat: agregar configuración de Waydroid y optimizaciones en el sistema Nix

--- nixos/local-packages.nix
+++ nixos/local-packages.nix
@@ -1,8 +1,13 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs.eden.enable = true;
   programs.firefox.enable = true;
+  virtualisation.waydroid = {
+    enable = true;
+    package = pkgs.waydroid-nftables;
+  };
   environment.systemPackages = with pkgs; [ 
     bleachbit
     gparted
+    waydroid-helper
   ];
 }
--- nixos/modules/tweaks.nix
+++ nixos/modules/tweaks.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 
 let 
   cfg = config.tweaks;
@@ -26,6 +26,18 @@ in
       settings = {
         auto-optimise-store = true;
       };
+    };
+
+    # Cache binaria de Nix
+    nix.settings = {
+      substituters = [ 
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org" 
+      ];
+      trusted-public-keys = [
+         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
     };
 
     # Eliminación de generaciones antiguas de Nix
